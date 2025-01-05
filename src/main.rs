706acc6c03ed1c5ef9a00758090fbe44e29300ab
Kawa_ <yuyux0910@gmail.com>
2025-01-05 23:06:15 +0900

    [add] ディレクトリを再帰的に探索する

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use clap::Parser;
 use std::fs;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Cli {
-    path: std::path::PathBuf,
+    path: PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
     println!("{:?}", args.path);
 
-    match fs::read_dir(args.path) {
+    recurse_folder(&args.path.to_path_buf());
+}
+
+fn recurse_folder(path: &PathBuf) {
+    match fs::read_dir(path) {
         Err(why) => println!("! {:?}", why.kind()),
         Ok(paths) => {
             for path in paths {
-                println!("> {:?}", path.unwrap().path());
+                let path_value = path.unwrap();
+                println!("> {:?}", path_value.file_name());
+                if path_value.path().is_dir() {
+                    recurse_folder(&path_value.path());
+                }
             }
         }
     }
