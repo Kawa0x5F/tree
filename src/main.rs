9137be0a3248c8b22d03fcb26fa6f93a3d78b368
Kawa_ <yuyux0910@gmail.com>
2025-01-05 23:46:03 +0900

    [add] 階層構造で出力する

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,39 @@ fn main() {
     let args = Cli::parse();
     println!("{:?}", args.path);
 
-    recurse_folder(&args.path.to_path_buf());
+    recurse_folder(args.path.to_path_buf(), 0);
 }
 
-fn recurse_folder(path: &PathBuf) {
+fn recurse_folder(path: PathBuf, level: usize) {
     match fs::read_dir(path) {
         Err(why) => println!("! {:?}", why.kind()),
         Ok(paths) => {
             for path in paths {
                 let path_value = path.unwrap();
-                println!("> {:?}", path_value.file_name());
+                println!(
+                    "{}{}",
+                    make_hierarchy(level),
+                    path_value.file_name().into_string().ok().unwrap()
+                );
                 if path_value.path().is_dir() {
-                    recurse_folder(&path_value.path());
+                    recurse_folder(path_value.path(), level + 1);
                 }
             }
         }
     }
+}
+
+fn make_hierarchy(level: usize) -> String {
+    let mut output_string = String::new();
+
+    if level == 0 {
+        return output_string;
+    }
+
+    for _i in 0..(level - 1) {
+        output_string.push_str("|  ");
+    }
+    output_string.push_str("|--");
+
+    output_string
 }
